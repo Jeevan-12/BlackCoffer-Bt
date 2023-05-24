0aa0104a93b1f7984fa3a3ff709c7c7c4e714445
Jeevan-12 <jeevangowdabjeeva@gmail.com>
2023-05-24 19:16:37 +0530

    change in backend

--- controller/controller.js
+++ controller/controller.js
@@ -19,4 +19,38 @@ const getinsert = async (req, resp) => {
     resp.status(400).json({ messsage: 'error' });
   }
 };
+
+module.exports = async (req, res) => {
+  await within(getinsert, res, 7000);
+};
+
+async function within(fn, res, duration) {
+  const id = setTimeout(
+    () =>
+      res.json({
+        message: 'There was an error with the upstream service!',
+      }),
+    duration
+  );
+
+  try {
+    let data = await fn();
+    clearTimeout(id);
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
+
+async function getinsert() {
+  try {
+    db = await connect();
+    const data = await db.find({}).toArray();
+
+    resp.status(200).json({ messsage: 'datasend', data });
+  } catch (e) {
+    resp.status(400).json({ messsage: 'error' });
+  }
+}
+
 module.exports = { getdata, getinsert };
